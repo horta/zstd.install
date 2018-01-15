78a41da415c8a8fab92bcbec265316e7c2f834c5
Danilo Horta <danilo.horta@gmail.com>
2018-01-15 21:34:34 +0000

    Install

--- install.bat
+++ install.bat
@@ -6,7 +6,7 @@ set FILE=zstd-%VERSION%.zip
 set DIR=zstd-%VERSION%
 set URL=https://github.com/facebook/zstd/archive/v%VERSION%.zip
 
-echo [0/5] Library(zstd==%VERSION%)
+echo [0/4] Library(zstd==%VERSION%)
 
 SET ORIGIN=%cd%
 SET LOG_FILE=%ORIGIN%\install.log
@@ -17,28 +17,23 @@ rd /S /Q %DIR% >nul 2>&1
 del /Q %LOG_FILE% ! >nul 2>&1
 copy /y nul %LOG_FILE% >nul 2>&1
 
-echo|set /p="[1/5] Downloading... "
+echo|set /p="[1/4] Downloading... "
 echo Fetching %URL% >>%LOG_FILE% 2>&1
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%URL%', '%FILE%')" >>%LOG_FILE% 2>&1
 if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
 
-echo|set /p="[2/5] Extracting... "
+echo|set /p="[2/4] Extracting... "
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%FILE%', '.'); }
 if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
 
 cd %DIR%\build\cmake && mkdir build && cd build
 
-echo|set /p="[3/5] Configuring... "
-cmake .. -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%programfiles%\zstd" .. >>%LOG_FILE% 2>&1
+echo|set /p="[3/4] Configuring... "
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%programfiles%\zstd" .. >>%LOG_FILE% 2>&1
 if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
 
-echo|set /p="[4/5] Compiling... "
-nmake >>%LOG_FILE% 2>&1
-if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
-
-echo|set /p="[5/5] Installing... "
-nmake install >>%LOG_FILE% 2>&1
-set PATH=%PATH%;%programfiles%\zstd\bin
+echo|set /p="[4/4] Compiling and installing... "
+cmake --build . --config Release --target install >>%LOG_FILE% 2>&1
 if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
 
 cd %ORIGIN% >nul 2>&1
