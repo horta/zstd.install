4ded40ae1b9cec1719cb0be1ef31e4f0e9a745e7
Danilo Horta <danilo.horta@gmail.com>
2018-02-28 18:34:44 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,10 @@
 @echo off
+SETLOCAL
+
+:: Log file
+SET ORIGIN=%cd%
+call :joinpath "%ORIGIN%" "install.log"
+SET LOG_FILE=%Result%
 
 :: Configuration
 set VERSION=1.3.3
@@ -9,9 +15,6 @@ IF "%ARCH%"=="" set ARCH=x64
 
 echo [0/4] Library(zstd==%VERSION%)
 
-SET ORIGIN=%cd%
-SET LOG_FILE=%ORIGIN%\install.log
-
 :: Cleaning up previous mess
 del /Q %FILE% ! >nul 2>&1
 rd /S /Q %DIR% >nul 2>&1
@@ -20,7 +23,7 @@ copy /y nul %LOG_FILE% >nul 2>&1
 
 echo|set /p="[1/4] Downloading... "
 echo Fetching %URL% >>%LOG_FILE% 2>&1
-powershell -Command "(New-Object Net.WebClient).DownloadFile('%URL%', '%FILE%')" >>%LOG_FILE% 2>&1
+call :winget "%URL%" >>%LOG_FILE% 2>&1
 if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
 
 echo|set /p="[2/4] Extracting... "
@@ -44,3 +47,40 @@ rd /S /Q %DIR% >nul 2>&1
 echo Details can be found at %LOG_FILE%.
 
 @echo on
+@goto :eof
+
+:joinpath
+set Path1=%~1
+set Path2=%~2
+if {%Path1:~-1,1%}=={\} (set Result=%Path1%%Path2%) else (set Result=%Path1%\%Path2%)
+goto :eof
+
+:winget    - download file given url
+set URL=%~1
+for %%F in (%URL%) do set FILE=%%~nxF
+
+setlocal EnableDelayedExpansion
+set multiLine=$security_protcols = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::SystemDefault^
+
+if ([Net.SecurityProtocolType].GetMember('Tls11').Count -gt 0) {^
+
+ $security_protcols = $security_protcols -bor [Net.SecurityProtocolType]::Tls11^
+
+}^
+
+if ([Net.SecurityProtocolType].GetMember('Tls12').Count -gt 0) {^
+
+$security_protcols = $security_protcols -bor [Net.SecurityProtocolType]::Tls12^
+
+}^
+
+[Net.ServicePointManager]::SecurityProtocol = $security_protcols^
+
+Write-Host 'Downloading %URL%... ' -NoNewLine^
+
+(New-Object Net.WebClient).DownloadFile('%URL%', '%FILE%')^
+
+Write-Host 'done.'
+
+powershell -Command !multiLine!
+goto:eof
